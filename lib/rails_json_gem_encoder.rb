d8f801ec020e9057205b63befbd93282b33c09e7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-11-01 12:52:37 +0900

    Avoid running `escape` twice

--- lib/rails_json_gem_encoder.rb
+++ lib/rails_json_gem_encoder.rb
@@ -6,7 +6,18 @@ require "active_support/json/encoding"
 class RailsJSONGemEncoder < ActiveSupport::JSON::Encoding::JSONGemEncoder
   private
 
+  class EscapedString < String
+    def to_json(*)
+      s = super
+      JSONEscaper.escape(s)
+    end
+
+    def to_s
+      self
+    end
+  end
+
   def jsonify(value)
-    value.is_a?(String) ? JSONEscaper.escape(value) : super
+    value.is_a?(String) ? EscapedString.new(value) : super
   end
 end
