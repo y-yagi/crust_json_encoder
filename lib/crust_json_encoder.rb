bacaf773c75adfeb8de52a5d0d8050e967af5308
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-11-06 08:43:31 +0900

    Correctly load built library

--- lib/crust_json_encoder.rb
+++ lib/crust_json_encoder.rb
@@ -1,9 +1,16 @@
 require "rutie"
 require "active_support"
 require "active_support/json"
+require "thermite/config"
 
 class CrustJSONEncoder < ActiveSupport::JSON::Encoding::JSONGemEncoder
-  Rutie.new(:native).init "Init_native", __dir__
+  LIB_PATH = begin
+    toplevel_dir = File.dirname(__dir__)
+    config = Thermite::Config.new(cargo_project_path: toplevel_dir, ruby_project_path: toplevel_dir)
+    config.ruby_path("lib")
+  end
+
+  Rutie.new(:native, lib_path: LIB_PATH, lib_prefix: nil).init "Init_native", __dir__
 
   private
 
