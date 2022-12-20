5313afe2f0fbec57255060dca61ea8c7626e3e89
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-12-20 13:02:25 +0900

    Try to load library a specific version first

--- lib/crust_json_encoder.rb
+++ lib/crust_json_encoder.rb
@@ -1,6 +1,11 @@
 require "active_support"
 require "active_support/json"
-require "crust_json_encoder/crust_json_encoder"
+
+begin
+  require "crust_json_encoder/#{RUBY_VERSION.to_f}/crust_json_encoder"
+rescue LoadError
+  require "crust_json_encoder/crust_json_encoder"
+end
 
 class CrustJSONEncoder < ActiveSupport::JSON::Encoding::JSONGemEncoder
   private
